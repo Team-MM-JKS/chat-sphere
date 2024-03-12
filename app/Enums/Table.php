2da97ea2e98af38303ef631f4f73609e9c315579
alj_Josephlian <josephlian@aljmyanmar.com>
2024-03-12 13:21:29 +0630

    [NEW] add messages and message_recipients tables at Enums

--- app/Enums/Table.php
+++ app/Enums/Table.php
@@ -7,4 +7,7 @@ class Table
 	const USER = 'users';
 	const USER_INFORMATION = 'user_information';
 	const GROUP = 'groups';
+	const USER_GROUP = 'user_groups';
+	const MESSAGE = 'messages';
+	const MESSAGE_RECIPIENT = 'message_recipients';
 }
